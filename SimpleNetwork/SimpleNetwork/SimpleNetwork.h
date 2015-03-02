9e259c5a716baeb54d97dd14a03e4927a2101ec4
董铂然 <dantesx2012@gmail.com>
2015-03-02 15:10:23 +0800

    在工作组中建立网络框架
    并且可成功运行

---
+++ SimpleNetwork/SimpleNetwork/SimpleNetwork.h
@@ -0,0 +1,19 @@
+//
+//  SimpleNetwork.h
+//  SimpleNetwork
+//
+//  Created by 董 尚先 on 15/3/2.
+//  Copyright (c) 2015年 shangxianDante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleNetwork.
+FOUNDATION_EXPORT double SimpleNetworkVersionNumber;
+
+//! Project version string for SimpleNetwork.
+FOUNDATION_EXPORT const unsigned char SimpleNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleNetwork/PublicHeader.h>
+
+
